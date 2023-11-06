6e0d6765009339d3d7375c62c1f9e4d2f0dc900e
Jose Aranguren <jmaranguren89@gmail.com>
2023-11-06 22:39:45 +0100

    Ejercicio 1 terminado con modificacion css

--- Ejercicios tema2.2/ejercicio1.php
+++ Ejercicios tema2.2/ejercicio1.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1 U2.2</title>
+
+    
+
 </head>
+
 <body>
 
-<?php
+    <?php
 
-/* 
+    /* 
 * Define tres arrays de 10 números enteros cada una, con nombres "numero",cuadrado"
 * y "cubo". Carga el array "numero" con valores aleatorios entre 0 y 100.
 * En el array "cuadrado" se deben almacenar los cuadrados de los valores que hay en el array "numero".
@@ -18,60 +23,53 @@
 *
 */
 
-$numero = [];
-$cuadrado = [];
-$cubo = [];
+    $numero = [];
+    $cuadrado = [];
+    $cubo = [];
 
-for ($i=0; $i < 10 ; $i++) { 
-    
-    $numero[$i]= rand(0,100);
-}
+    for ($i = 0; $i < 10; $i++) {
 
-for ($i=0; $i < 10 ; $i++) { 
-    
-    $cuadrado[$i] = pow($numero[$i],2);
-}
+        $numero[$i] = rand(0, 100);
+    }
 
-for ($i=0; $i < 10 ; $i++) { 
-    
-    $cubo[$i] = pow($numero[$i],3);
-}
+    for ($i = 0; $i < 10; $i++) {
 
-tabla($numero,$cuadrado,$cubo);
+        $cuadrado[$i] = pow($numero[$i], 2);
+    }
+
+    for ($i = 0; $i < 10; $i++) {
+
+        $cubo[$i] = pow($numero[$i], 3);
+    }
+
+    tabla($numero, $cuadrado, $cubo);
 
 
 
-function tabla   ($numero, $cuadrado, $cubo){
+    function tabla($numero, $cuadrado, $cubo)
+    {
 
-    print('<table border="1">
+        print('<table class="tablacss" border="1">
         <thead>
             <th>Numero</th>
             <th>Cuadrado</th>
             <th>Cubo</th>
         </thead>');
 
-    for ($i=0; $i < 10 ; $i++) { 
-        
-        printf ('<tr>
+        for ($i = 0; $i < 10; $i++) {
+
+            printf('<tr>
             <td>%d</td>
             <td>%d</td>
             <td>%d</td>
-        </tr>',$numero[$i],$cuadrado[$i],$cubo[$i]);
-       
+        </tr>', $numero[$i], $cuadrado[$i], $cubo[$i]);
+        }
+
+        print('</table>');
     }
 
-    print ('</table>');
+    ?>
 
-}
-
-
-
-
-
-
-
-
-?>
-    
 </body>
+
 </html>
