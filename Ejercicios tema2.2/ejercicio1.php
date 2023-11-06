8c76c07c539b4200b4ae0509423d25e35f8d178c
Jose Aranguren <jmaranguren89@gmail.com>
2023-11-06 22:19:53 +0100

    Ejercicio 2.1 Hecho

--- Ejercicios tema2.2/ejercicio1.php
+++ Ejercicios tema2.2/ejercicio1.php
@@ -9,6 +9,65 @@
 
 <?php
 
+/* 
+* Define tres arrays de 10 números enteros cada una, con nombres "numero",cuadrado"
+* y "cubo". Carga el array "numero" con valores aleatorios entre 0 y 100.
+* En el array "cuadrado" se deben almacenar los cuadrados de los valores que hay en el array "numero".
+* En el array "cubo" se deben almacenar los cubos de los valores que hay en "numero".
+* A continuación, muestra el contenido de los tres arrays dispuestos en tres columnas.
+*
+*/
+
+$numero = [];
+$cuadrado = [];
+$cubo = [];
+
+for ($i=0; $i < 10 ; $i++) { 
+    
+    $numero[$i]= rand(0,100);
+}
+
+for ($i=0; $i < 10 ; $i++) { 
+    
+    $cuadrado[$i] = pow($numero[$i],2);
+}
+
+for ($i=0; $i < 10 ; $i++) { 
+    
+    $cubo[$i] = pow($numero[$i],3);
+}
+
+tabla($numero,$cuadrado,$cubo);
+
+
+
+function tabla   ($numero, $cuadrado, $cubo){
+
+    print('<table border="1">
+        <thead>
+            <th>Numero</th>
+            <th>Cuadrado</th>
+            <th>Cubo</th>
+        </thead>');
+
+    for ($i=0; $i < 10 ; $i++) { 
+        
+        printf ('<tr>
+            <td>%d</td>
+            <td>%d</td>
+            <td>%d</td>
+        </tr>',$numero[$i],$cuadrado[$i],$cubo[$i]);
+       
+    }
+
+    print ('</table>');
+
+}
+
+
+
+
+
 
 
 
