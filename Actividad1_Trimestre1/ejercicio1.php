a25892c67138429e31192c0faca906e29d2d98f7
Jose Aranguren <jmaranguren89@gmail.com>
2023-11-18 15:00:07 +0100

    Actividad 1 ejercicio 1 hecho

---
+++ Actividad1_Trimestre1/ejercicio1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+
+</head>
+<body>
+
+<?php
+
+// Realizar un programa PHP que generé 2 números aleatorios (entre 5 y 15) y me dibujé un
+// rectángulo de asteriscos como se puesta en la figura:
+
+//Variables
+define('MAX_ALTO', 15);
+define('MAX_ANCHO', 15);
+define('MIN_ALTO', 5);
+define('MIN_ANCHO', 5);
+
+$dimensiones= dimensioniza (MAX_ALTO, MAX_ANCHO, MIN_ALTO, MIN_ANCHO);
+
+
+
+//Titulo
+print("<h1>Ejercicio rectangulo aleatorio<h1>");
+    print("<h3> El rectangulo es de: {$dimensiones['ancho']} x {$dimensiones['alto']}</h3>");
+
+//Dibujamos rectangulo
+for ($i=0; $i < $dimensiones['alto'] ; $i++) { 
+    
+    for ($j=0; $j <$dimensiones['ancho'] ; $j++) { 
+        print("*");
+    }
+
+    echo ('<br>');
+}
+
+//Recibe alto y ancho maximo para generar un rectangulo con dimensiones aleatorias entre los valores proporcionados
+function dimensioniza ($maxAlto,$maxAncho,$minAlto,$minAncho) {
+
+    $anchoAleatorio =rand($minAncho,$maxAncho);
+    $altoAleatorio =rand($minAlto,$maxAlto);
+    $dimensiones= ['ancho' => $anchoAleatorio, 'alto' => $altoAleatorio];
+
+    return $dimensiones;
+}
+
+
+
+?>
+
+
+</body>
+</html>
