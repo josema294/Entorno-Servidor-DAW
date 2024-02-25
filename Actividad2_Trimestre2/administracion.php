17ba1bac6342bd1414209429904b08de0f70dc44
Jose Aranguren <jmaranguren89@gmail.com>
2024-02-25 01:52:10 +0100

    Terminado panel de adminsitracion

--- Actividad2_Trimestre2/administracion.php
+++ Actividad2_Trimestre2/administracion.php
@@ -4,53 +4,196 @@
 
 <main>
 
-<div class="container-fluid">
-    <div class="row">
-        <!-- Barra lateral -->
-        <div class="col-md-2 d-flex flex-column flex-shrink-0 p-3 bg-light" style="height: 100vh;">
-            <h4>Panel de Administración</h4>
-            <ul class="nav nav-pills flex-column mb-auto">
-                <li class="nav-item">
-                    <a href="#" class="nav-link active" aria-current="page">Inicio</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link link-dark">Gestión de Usuarios</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link link-dark">Gestión de Pisos</a>
-                </li>
-            </ul>
-        </div>
+    <div class="container-fluid">
+        <div class="row">
+          
 
-        <!-- Contenido principal -->
-        <div class="col-md-10 p-4">
-            <h1>Bienvenido al Panel de Administración</h1>
-            <hr>
-            
-            <h2>Gestión de Usuarios</h2>
-            <div class="mb-3">
-                <!-- Formulario de búsqueda para usuarios -->
-                <form class="d-flex mb-4" action="ruta_para_procesar_busqueda_usuarios.php" method="GET">
-                    <input class="form-control me-2" type="search" placeholder="Buscar usuario" aria-label="Buscar" name="busquedaUsuario">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-                <a href="./publicarUser.php" class="btn btn-success mb-2">Creacion de usuario</a>
-                <button class="btn btn-info mb-2">Listar</button>
-            </div>
-            
-            <h2>Gestión de Pisos</h2>
-            <div>
-                <!-- Formulario de búsqueda para pisos -->
-                <form class="d-flex mb-4" action="ruta_para_procesar_busqueda_pisos.php" method="GET">
-                    <input class="form-control me-2" type="search" placeholder="Buscar piso" aria-label="Buscar" name="busquedaPiso">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-                <a href="./publicaPiso.php" class="btn btn-success mb-2">Creacion de piso</a>
-                <button class="btn btn-info mb-2">Listar</button>
+            <!-- Contenido principal -->
+            <div class="col-md-10 p-4">
+                <h1>Bienvenido al Panel de Administración</h1>
+                <hr>
+
+                <h2>Gestión de Usuarios</h2>
+                <div class="mb-3">
+                    <!-- Formulario de búsqueda para usuarios -->
+                    <form class="d-flex mb-4" action="./administracion.php" method="GET">
+                        <input class="form-control me-2" type="search" placeholder="Buscar usuario por su id" aria-label="Buscar" name="busquedaUsuario">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                    <a href="./publicarUser.php" class="btn btn-success mb-2">Creacion de usuario</a>
+                    <form class="d-flex mb-4" action="./administracion.php" method="GET">
+                        <input class="form-control me-2" type="hidden" name="listarUsuarios" value="all">
+                        <button class="btn btn-info mb-2" type="submit">Listar</button>
+                    </form>
+                </div>
+
+                <h2>Gestión de Pisos</h2>
+                <div>
+                    <!-- Formulario de búsqueda para pisos -->
+                    <form class="d-flex mb-4" action="#" method="GET">
+                        <input class="form-control me-2" type="search" placeholder="Buscar piso por id" aria-label="Buscar" name="busquedaPiso">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                    <a href="./publicaPiso.php" class="btn btn-success mb-2">Creacion de piso</a>
+                    <form class="d-flex mb-4" action="./administracion.php" method="GET">
+                        <input class="form-control me-2" type="hidden" name="listarPisos" value="all">
+                        <button class="btn btn-info mb-2" type="submit">Listar</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+
+    <br>
+    <br>
+
+    <?php
+    include("./config/db.php");
+
+    $modoImpresion = ""; //Define si se van a imprimir pisos o usuarios ya que tienen plantillas diferentes
+
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        DataBaseConection::openConection();
+        $conexion =  DataBaseConection::getConexion();
+
+        //Distinguimos si la busqueda es de casa o usuario
+
+        if (isset($_GET["busquedaUsuario"])) {
+
+            $sql = "SELECT * from USUARIOS WHERE usuario_id = {$_GET["busquedaUsuario"]}";
+            $result = mysqli_query($conexion, $sql);
+            $modoImpresion = "usuarios";
+        }
+
+        if (isset($_GET["listarUsuarios"])) {
+
+            $sql = "SELECT * from USUARIOS ";
+            $result = mysqli_query($conexion, $sql);
+            $modoImpresion = "usuarios";
+        }
+
+        if (isset($_GET["busquedaPiso"])) {
+            $sql = "SELECT * from pisos WHERE Codigo_piso = {$_GET["busquedaPiso"]} ";
+            $result = mysqli_query($conexion, $sql);
+            $modoImpresion = "pisos";
+        }
+
+        if (isset($_GET["listarPisos"])) {
+            $sql = "SELECT * from pisos";
+            $result = mysqli_query($conexion, $sql);
+            $modoImpresion = "pisos";
+        }
+        
+        //Pintamos los resultados para Usuarios
+        if ($modoImpresion == "usuarios") {
+
+            print('
+            <div
+                class="table-responsive"
+            >
+                <table
+                    class="table table-primary"
+                >
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Correo</th>
+                            <th scope="col">Clave</th>
+                            <th scope="col">Tipo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+        
+                    ');
+
+            for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+                $arrayResul = mysqli_fetch_assoc($result);
+                $id =  $arrayResul["usuario_id"];
+                $nombre = $arrayResul["nombres"];
+                $correo = $arrayResul["correo"];
+                $clave = $arrayResul["clave"];
+                $tipo = $arrayResul["tipo_usuario"];
+
+                printf('<tr class="">
+                        <td scope="row">%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                    </tr>', $id, $nombre, $correo, $clave, $tipo);
+            }
+            print(
+                '
+                        
+        
+                        </tbody>
+                    </table>
+                </div>');
+        }
+
+        //Pintamos los resultados para Pisos
+        if ($modoImpresion == "pisos") {
+            print('
+            <div
+                class="table-responsive"
+            >
+                <table
+                    class="table table-primary"
+                >
+                    <thead>
+                        <tr>
+                            <th scope="col">Codigo_piso</th>
+                            <th scope="col">Direccion</th>
+                            <th scope="col">Metros</th>
+                            <th scope="col">Zona</th>
+                            <th scope="col">Precio</th>
+                            <th scope="col">Imagen</th>
+                            <th scope="col">Id propietario</th>
+        
+                        </tr>
+                    </thead>
+                    <tbody>
+        
+                    ');
+
+            for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+                $arrayResul = mysqli_fetch_assoc($result);
+                $id =  $arrayResul["Codigo_piso"];
+                $calle = $arrayResul["calle"];
+                $numero = $arrayResul["numero"];
+                $piso = $arrayResul["piso"];
+                $puerta = $arrayResul["puerta"];
+                $cp = $arrayResul["cp"];
+                $direccion = $calle . " " . $numero . " " . $piso . " " . $puerta . " " . $cp;
+                $metros = $arrayResul["metros"];
+                $zona = $arrayResul["zona"];
+                $precio = $arrayResul["precio"];
+                $imagen = $arrayResul["imagen"];
+                $propietario = $arrayResul["usuario_id"];
+
+                printf('<tr class="">
+                        <td scope="row">%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td>%s</td>
+                        <td><a href="%s"> Enlace a la imagen</a></td>
+                        <td>%s</td>
+                    </tr>', $id, $direccion, $metros, $zona, $precio, $imagen, $propietario);
+            }
+            print(
+                '
+                        
+        
+                        </tbody>
+                    </table>
+                </div>');
+        }
+    }
+
+    ?>
 
 
 </main>
