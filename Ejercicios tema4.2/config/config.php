831d47010704a831052f11bbdccf9d44bc4cd04f
Jose Aranguren <jmaranguren89@gmail.com>
2024-03-17 10:28:52 +0100

    ejer 4.2 modo pruebas = false

--- Ejercicios tema4.2/config/config.php
+++ Ejercicios tema4.2/config/config.php
@@ -3,7 +3,7 @@
 class Pruebas {
     
     //Cambia esta variable si quieres cambiar de pruebas a produccion para la base de datos.
-    private static $entornoPruebas = true;
+    private static $entornoPruebas = false;
 
     public static function entornoPruebas() : bool {
         
