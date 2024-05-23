664eec8fa5a6e627dcd2c5d1f544d5c52c2c74e0
josema294 <jmaranguren89@gmail.com>
2024-05-23 16:55:55 +0200

    estructura basica de examen y simulacro

---
+++ Simulacros/PhpSimulacro/ejercicio1.php
@@ -0,0 +1,30 @@
+<!-- Hacer una aplicación en PHP que para un colegio: (3.5 puntos). 
+El ejercicio se debe realizar con ficheros de texto. 
+ 
+Cree un archivo de texto llamado "notas.txt" si no existe. 
+Implementa un menú de opciones para que el usuario elija qué acción realizar. 
+Permita al usuario realizar las siguientes acciones: 
+1. Agregar un nuevo estudiante junto con su nota. 
+Para ello usaremos un formulario con el formato que se desee. 
+2. Mostrar todas las notas guardadas en el archivo. 
+Cada entrada de estudiante y nota debe estar en el formato "nombre:nota". 
+Después de cada acción, hacer un enlace para volver el menú principal.  
+ -->
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+
+
+
+
+
+    
+ </body>
+ </html>
